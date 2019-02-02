8e5e76ceef9f5a3dfe38c5bf142fe3e09fec931d
bilbobx182 <c14474048@mydit.ie>
2019-02-02 10:13:07 +0000

    Implemented getting company addresses

--- get_list_of_recruiters.py
+++ get_list_of_recruiters.py
@@ -2,10 +2,21 @@ import json
 import os
 from os import listdir
 from os.path import isfile, join
+import geocoder
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
 
 cwd = os.getcwd()
 path = cwd + "\\data"
 allfiles = [f for f in listdir(path) if isfile(join(path, f))]
+
+
+# TODO Get address when company has many locations in dublin
+# TODO Append to address to file
+# TODO Deal with duplicates Bank of America vs Bank of America Meriyl Lynch Susquehanna International Group VS SIG
 
 
 def get_recruiters():
@@ -17,6 +28,18 @@ def update_recruiters_json(data):
     with open(cwd + '\\recruiters.json', 'w') as outfile:
         json.dump(data, outfile)
 
+def get_address_from_google(company):
+    baseurl = "https://www.google.com/search?client=firefox-b-ab&q="
+    url = baseurl + "+" + company + "+" + "Dublin address"
+    request_result = requests.get(url)
+    soup = BeautifulSoup(request_result.content, "html.parser")
+    result = soup.find_all("span", {"class": "A1t5ne"})
+
+    try:
+        print(company + " , " + result[0].contents[0].replace(",",""))
+    except:
+        print(company + ", NA")
+    time.sleep(7.5)
 
 def get_list_of_recruiters_from_irish_jobs():
     # Irish jobs allows us to query recruiters only, we get a list of recrutiers from here.
@@ -38,18 +61,31 @@ def get_list_of_recruiters_from_irish_jobs():
 
 def get_hiring_companies():
     companies = []
+    dont_search_companies = []
+
     for file in allfiles:
         with open(path + "\\" + file, mode='r') as csv_file:
+
             for line in csv_file.readlines():
                 company = line.split(",")[1].strip()
                 if (company not in get_recruiters()['recruiters']):
                     if (company not in companies):
                         companies.append(company)
-    print(companies)
 
+    print(len(companies))
+
+    with open(cwd + "\\" + "address.csv", mode='r') as csv_file:
+        for line in csv_file.readlines():
+            dont_search_companies.append(line.split(",")[0].strip())
+
+    for company in companies:
+        if company not in dont_search_companies:
+            get_address_from_google(company.replace("ltd",""))
 
 def main():
     get_list_of_recruiters_from_irish_jobs()
     get_hiring_companies()
+    # get_company_geo_data()
+
 
 main()
---
+++ massage_data.py
@@ -0,0 +1,11 @@
+def removeDuplicateJobs():
+    print(" removeDuplicateJobs")
+
+def removeDuplicatePropertyByAddress():
+    print("byAddress")
+
+def removeDuplicatePropertyByLatAndLong():
+    print("removebyLatLong")
+
+def removePropertyOutsideDublin():
+    print("Outside")
